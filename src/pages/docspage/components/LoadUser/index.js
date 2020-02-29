1a4bb16e204aeb579c98164492b2dfe6ee576b2e
kinshuk <kinshuk2jain@gmail.com>
2020-02-29 23:02:07 +0100

    load user modal

--- src/pages/docspage/components/LoadUser/index.js
+++ src/pages/docspage/components/LoadUser/index.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import { fetchApi } from '../../../../utils/fetch';
 import '../common.css'
 
-export const LoadUser = () => {
-  return (
-    <div>
-      <div className="user-input-field">
-        <p className="user-input-field-input">Load User</p>
+export class LoadUser extends React.Component {
+  state = {
+    userList: [],
+    fetchedWithPrefix: '',
+    name: ''
+  }
+
+  onChangeHandler = (e) => {
+    const prefix = e.target.value.trim();
+    const { userList, fetchedWithPrefix } = this.state;
+    if(prefix.length >= 3) {
+      if (!userList.length || !prefix.startsWith(fetchedWithPrefix)) {
+        fetchApi('/get-names', {
+          method: 'POST',
+          body: JSON.stringify({
+            prefix
+          })
+        })
+        .then(resp => {
+          this.setState({ userList: resp.suggestions, fetchedWithPrefix: prefix })
+        })
+      }
+    }
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="user-input-field">
+          <p className="user-input-field-input">Load User</p>
+        </div>
+        <div className="user-input-field">
+          <input onChange={this.onChangeHandler} placeholder="name *" className="user-input-field-input" />
+        </div>
+        <div className="user-input-field">
+          <button className="user-input-field-button">Submit</button>
+        </div>
       </div>
-      <div className="user-input-field">
-        <input placeholder="name *" className="user-input-field-input" />
-      </div>
-      <div className="user-input-field">
-        <button className="user-input-field-button">Submit</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
