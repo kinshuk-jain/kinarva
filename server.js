146d2ef291e95d4919cb3a7659e7d77aa1ccb4ea
kinshuk <kinshuk2jain@gmail.com>
2020-04-26 19:32:58 +0200

    Updating react-scripts

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 // we use node to serve the project in prod
+const express = require('express')
+const path = require('path')
+const app = express()
+const helmet = require('helmet')
 
-if (process.env.NODE_ENV === 'production') {
-  const express = require('express')
-  const path = require('path')
-  const app = express()
-  const helmet = require('helmet')
+app.use(helmet())
 
-  app.use(helmet())
+app.use(express.static(path.join(__dirname, 'build')))
 
-  app.use(express.static(path.join(__dirname, 'build')))
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  })
-
-  app.listen(3000)
-}
+// app.listen(3000)
